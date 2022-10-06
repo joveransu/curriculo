9932459b7d71bb42666c8c33c77a060d45f65964
Anderson Alencar <joveransu@gmail.com>
2022-10-06 17:39:56 -0300

    mobile 90%

---
+++ scripts/changeText.js
@@ -0,0 +1,9 @@
+function showMe(){
+    document.getElementById('skills').style.display = 'none'
+    document.getElementById('my-history').style.display = 'block'
+}
+
+function showSkills(){
+    document.getElementById('skills').style.display = 'block'
+    document.getElementById('my-history').style.display = 'none'
+}
